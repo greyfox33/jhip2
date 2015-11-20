6a04026d48e5d8664e6e256687d7f50edb1312ef
greyfox33 <robb@oakts.com>
2015-11-20 14:53:11 -0800

    fixes to composite

--- jhip2/src/main/webapp/scripts/app/custom/composite.controller.js
+++ jhip2/src/main/webapp/scripts/app/custom/composite.controller.js
@@ -1,35 +1,26 @@
 'use strict';
 
 angular.module('jhip2App').controller('CompositeController',
-    ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+    ['$scope', '$http', '$stateParams', 'Cwcase', 'Child', function($scope, $http, $stateParams, Cwcase, Child) {
 
-    	//
-        $scope.cwcase = [];
-        //$scope.cwcase = entity;
-        $scope.loadcase = function(id) {
-        	$http.get("http://localhost:3000/#/cwcase/" + id)
-        	.success(function(response)
-        			{$scope.cwcase = response.records;}
-        	);
+        $scope.cwcases = [];
+        // this is really bad, needs to be cleaned up to not hard code first array. Get rid of repeat in the composite.html
+        $scope.load = function(id) {
+        	Cwcase.get({id: $stateParams.id}, function(result) {
+                $scope.cwcases[0] = result;
+            });
         };
+        $scope.load($stateParams.id);
         
-//      Cwcase.get( {id: $stateparams.id}, function (result) {
-//      $scope.cwcase = result;
-//   });
-//        $scope.load = function (id) {
-//            Child.get({id: id}, function(result) {
-//                $scope.child = result;
-//            });
-//        };
-        $scope.loadcase($stateParams.id);
-        
+
+  
         $scope.children = [];
-        $scope.loadChildren = function(id) {
-        	$http.get("http://localhost:8080/childByCase")
-        	.success(function(response)
-        			{$scope.children = response.records;}
-        	);
+        $scope.loadChildren = function() {
+            Child.query(function(result) {
+               $scope.children = result;
+            });
         };
+        $scope.loadChildren();
         // add a refresh or clean here
 //        $scope.refresh = function () {
 //            $scope.loadAll();
